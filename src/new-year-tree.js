5e5fe0d5a08f9df687bb3599409161901a469c09
Eugene Lapin <kolduras@gmail.com>
2026-01-07 20:05:06 +0300

    Minify sources

---
+++ src/new-year-tree.js
@@ -0,0 +1,35 @@
+b.style.background=`#000`;
+
+r = [];
+o = [];
+x = m = 187;
+y = n = 32;
+q = 0;
+
+for (i = 0; i < 384; i++) {
+    o[i] = Math.round(Math.random() * .53);
+    r[i] = o[i] ? 0.4 : Math.random();
+};
+
+setInterval(() => {
+    q += .02;
+    q %= 2 * Math.PI;
+    c.fillStyle = `#000`;
+    c.rect(0, 0, 374, 512);
+    c.fill();
+
+    for (i = 0; i < 384; i++) {
+        t = .1 * i;
+        s = Math.sin(q + t);
+        x = m + 3 * t * Math.cos(q + t);
+        y = i + n + t * s++;
+        c.fillStyle = o[i]
+            ? `rgba(255,255,0,${s * .8})`
+            : `rgba(0,112,32,${s * .3 * r[i]})`;
+        c.moveTo(x, y);
+        c.beginPath();
+        c.arc(x, y, 28 * r[i], 0, 2 * Math.PI, 1);
+        c.fill();
+        c.closePath();
+    }
+}, 16);
